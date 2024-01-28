8340e9efc846936c0b337cc33a16739a1c73b58b
Luis Gustavo <luisgustavomacedo13@gmail.com>
2024-01-28 17:36:23 -0300

    parse js code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
+#!/usr/bin/env node
 import { parseArgs } from "node:util"
+import fs from "node:fs"
 import chalk from 'chalk';
+import * as espree from "espree"
 
 function getFilePathFromCLI() {
   try {
@@ -17,8 +20,16 @@ function getFilePathFromCLI() {
     return file;
   } catch (error) {
     console.error(chalk.red('Error: Missing file path. Please provide a file path using the -f or --file flag.'));
+    process.exit(1);
   }
 }
 
 const filePath = getFilePathFromCLI();
-console.log(filePath); 
+const code = fs.readFileSync(filePath, 'utf-8');
+const ast = espree.parse(code, { 
+  ecmaVersion: 2020,
+  loc: true,
+  sourceType: 'module'
+});
+
+console.log(JSON.stringify(ast, null, 2))
