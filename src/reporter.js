ea8df7ecf25a6d7f506a1301d489fc97d67241f0
Luis Gustavo <luisgustavomacedo13@gmail.com>
2024-01-29 07:06:24 -0300

    reporter class documentation

--- src/reporter.js
+++ src/reporter.js
@@ -4,6 +4,12 @@ import fs from "node:fs"
 import path from 'node:path';
 
 export class Reporter {
+  /**
+   * @param {object} `options` - The options object
+   * @param {Map<string, { message: string, errorLocation: string }>} `options.errors` - A map of errors with their location 
+   * @param {ASTNode} `options.ast` The "Program" AST node.
+   * @param {string} `options.outputFilePath` The output file path.
+   */
   static report({ errors, ast, outputFilePath }) {
     errors.forEach(({ message, errorLocation }) => {
       const errorMessage = `${chalk.red('Error:')} ${message}`;
@@ -11,18 +17,18 @@ export class Reporter {
       console.error(finalMessage);
       const updatedCode = astring.generate(ast);  
       fs.writeFileSync(outputFilePath, updatedCode, "utf-8");
-
-      if (!errors.length) {
-        console.log(chalk.green('Linting completed without errors'))
-      } else {
-        console.log(chalk.red(`Linting completed with ${errors.length} error(s)`))
-      }
-
-      console.log(
-        chalk.green('\nCode fixed and saved at'),
-        chalk.yellow('./' + path.basename(outputFilePath)),
-        chalk.green("successfully!")
-      )
     })
+
+    if (!errors.size) {
+      console.log(chalk.green('Linting completed without errors'))
+    } else {
+      console.log(chalk.red(`Linting completed with ${errors.size} error(s)`))
+    }
+
+    console.log(
+      chalk.green('\nCode fixed and saved at'),
+      chalk.yellow('./' + path.basename(outputFilePath)),
+      chalk.green("successfully!")
+    )
   }
 }
